1d87303214be060e53d0686f00a962e00fe4e4f8
zkfriendly <zkfriendly@proton.me>
2024-12-16 13:34:45 +0100

    feat: 4337 bundler and compatible node for dev environment

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,6 +2,11 @@ import "@nomicfoundation/hardhat-toolbox";
 
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    dev: {
+      url: "http://localhost:8545",
+    },
+  },
   paths: {
     sources: "./src",
     tests: "./test/hardhat",
--- contracts/test/hardhat/EmailAccount.t.ts
+++ contracts/test/hardhat/EmailAccount.t.ts
@@ -1,4 +1,33 @@
+import { expect } from "chai";
+
+import { Address, EmailAccount, EmailAccountFactory } from "../../typechain-types";
+import { ethers } from "hardhat";
+
 describe("EmailAccount", () => {
-    it("setup", () => {
+    let emailAccountFactory: EmailAccountFactory;
+    let emailAccount: EmailAccount;
+    let entryPoint: Address;
+
+    before(async () => {
+        const bundlerProvider = new ethers.JsonRpcProvider(
+            "http://localhost:3000/rpc"
+        );
+
+        // get list of supported entrypoints
+        const entrypoints = await bundlerProvider.send(
+            "eth_supportedEntryPoints",
+            []
+        );
+
+        if (entrypoints.length === 0) {
+            throw new Error("No entrypoints found");
+        }
+
+        entryPoint = entrypoints[0];
+
+    })
+
+    it("should have an entrypoint", () => {
+        expect(entryPoint).to.not.be.null;
     });
 });
