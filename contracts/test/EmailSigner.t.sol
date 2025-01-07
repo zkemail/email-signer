81b0bc926321b520efe778100cc40764bb43c640
zkfriendly <zkfriendly@proton.me>
2025-01-07 13:55:31 +0100

    refactor: remove unused library

--- contracts/test/EmailSigner.t.sol
+++ contracts/test/EmailSigner.t.sol
@@ -8,43 +8,6 @@ import "@zk-email/ether-email-auth-contracts/src/utils/Verifier.sol";
 import "@zk-email/contracts/UserOverrideableDKIMRegistry.sol";
 import "@zk-email/ether-email-auth-contracts/src/EmailAuth.sol";
 
-/**
- * @title SignatureDecoder - Decodes signatures encoded as bytes
- * @author Richard Meissner - @rmeissner
- */
-library SignatureDecoder {
-    /**
-     * @notice Splits signature bytes into `uint8 v, bytes32 r, bytes32 s`.
-     * @dev Make sure to perform a bounds check for @param pos, to avoid out of bounds access on @param signatures
-     *      The signature format is a compact form of {bytes32 r}{bytes32 s}{uint8 v}
-     *      Compact means uint8 is not padded to 32 bytes.
-     * @param pos Which signature to read.
-     *            A prior bounds check of this parameter should be performed, to avoid out of bounds access.
-     * @param signatures Concatenated {r, s, v} signatures.
-     * @return v Recovery ID or Safe signature type.
-     * @return r Output value r of the signature.
-     * @return s Output value s of the signature.
-     */
-    function signatureSplit(
-        bytes memory signatures,
-        uint256 pos
-    ) internal pure returns (uint8 v, bytes32 r, bytes32 s) {
-        // solhint-disable-next-line no-inline-assembly
-        assembly {
-            let signaturePos := mul(0x41, pos)
-            r := mload(add(signatures, add(signaturePos, 0x20)))
-            s := mload(add(signatures, add(signaturePos, 0x40)))
-            /**
-             * Here we are loading the last 32 bytes, including 31 bytes
-             * of 's'. There is no 'mload8' to do this.
-             * 'byte' is not working due to the Solidity parser, so lets
-             * use the second best option, 'and'
-             */
-            v := and(mload(add(signatures, add(signaturePos, 0x41))), 0xff)
-        }
-    }
-}
-
 contract EmailSignerTest is Test {
     EmailSigner emailSigner;
     EmailSignerFactory factory;
